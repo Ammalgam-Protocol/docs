6ca7019e377e2c59870a999f7e5cdcd5442b8b06
Will Fey <rev3ks@gmail.com>
2023-10-26 13:33:51 -0600

    added analytics tracker

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,10 +50,14 @@ const config = {
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
         //   editUrl:
-        //     'https://github.com/ammalgam-protocol/tree/main/packages/create-docusaurus/templates/shared/',
+        //     // 'https://github.com/ammalgam-protocol/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-2EZK5Z57G9',
+          anonymizeIP: true,
         },
       }),
     ],
